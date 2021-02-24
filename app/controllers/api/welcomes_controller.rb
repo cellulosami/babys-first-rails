e07ce911ff0dcf75e523c86a79f69aab0ec8561e
austin shields <ajshields196@gmail.com>
2021-02-23 19:08:03 -0500

    add hello and about paths and views

--- app/controllers/api/welcomes_controller.rb
+++ app/controllers/api/welcomes_controller.rb
@@ -1,2 +1,11 @@
 class Api::WelcomesController < ApplicationController
+  def hello
+    @random = Random.new.rand(100)
+    render "hello.json.jb"
+  end
+
+  def about
+    @favorite = "Ruby."
+    render "about.json.jb"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,11 @@ Rails.application.routes.draw do
   # namespace :api do
   #   get "/photos" => "photos#index"
   # end
+  namespace :api do
+    get "/welcome_path" => "welcomes#hello"
+  end
+
+  namespace :api do 
+    get "/about_path" => "welcomes#about"
+  end
 end
